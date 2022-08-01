582b3f4b948797e59ebb7edc0ea91c8597ac25db
Cian Butler <ciandevelops@gmail.com>
2022-08-01 08:57:20 -0400

    api working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { POKEMON_API_URL } from "./config";
 
 
 function App() {
+  useEffect(() => {
+    axios.get(`${POKEMON_API_URL}?limit=151`)
+      .then((response) => {
+        if(response.status === 200 && response.status <= 300){
+          console.log(response.data.results);
+        }
+      })
+    
+  }, [])
+  
   return (
     <div className="App">
      <h1>Hello World</h1>
